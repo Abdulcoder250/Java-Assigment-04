f5117d71ef9cf0f0cb94f594c7d79eff064784dc
Abdulcoder250 <abdulrafayy1020@gmail.com>
2025-10-27 16:58:36 +0500

    Add files via upload

---
+++ ASSIGMENT 21 JS/app.js
@@ -0,0 +1,430 @@
+                                        // Chapter # 9 
+                                         // Question 1
+                                        
+// var name =prompt ("enter ur City")
+
+// if (name == "karachi")
+// {
+//     document.writeln("Welcome To City Of Lights")
+// }
+
+// else
+// {
+//     document.writeln("Come To Karachi")
+// }
+
+                                        // Chap No 9 
+                                        // Question 2
+                                        
+// var name =prompt ("enter ur gender")
+
+// if (name == "Male")
+// {
+//     document.writeln("Hello Sir")
+// }
+
+// else (name == "Female")
+// {
+//     document.writeln("Hello Mam")
+// }
+
+                                    // Chapter # 9
+                                    // question 3
+
+// var signal1 =prompt ("enter ur Signal")
+
+// if (signal1 == "red")
+// {
+//     document.writeln("Wait")
+// }
+
+// else if (signal1 == "yellow")
+// (
+//     document.writeln("Start Engine")
+// )
+
+// else if (signal1 == "green")
+// (
+//     document.writeln("Bhag Jao")
+// )
+
+
+                                    // Chapter # 9
+                                    // Question 4
+                           
+// var fuel =prompt ("Choose Ur Fuel Left ==> 10L , 5L , 0.25L")
+
+// if (fuel == "10L")
+// {
+//     document.writeln("Jao Gari Bhaga K Aao")
+// }
+
+// else if (fuel == "5L")
+// (
+//     document.writeln("Zyada Door Mat Jana")
+// )
+
+// else if (fuel == "0.25")
+// (
+//     document.writeln("Fuel Bhara Lo GareebO")
+// )
+
+                                        // Chapter # 9 
+                                        // Question 5
+
+// var a = prompt("enter num")
+
+// if(++a === 5){
+//     document.writeln(" given condition for var is true ")
+// }
+
+
+// B 
+
+
+
+// var b =prompt("82") ; 
+// if (b++ === 83){
+//     document.writeln("given condition for var is true");
+// }
+
+
+// c 
+
+
+// var c =12;
+
+// if (c++ === 13){
+//     alert("condition 1 is True")
+// } 
+
+// if (c === 13){
+//     alert("condition 2 is True")
+// } 
+
+// if (++c < 14 )
+//  {
+//     alert ("condition 3 is True")
+//  }
+                       
+//  if (c === 14){
+//     alert("condition 4 is True")
+// }
+
+
+// D 
+
+// var materialCost = ("samna ki Cost ");
+// var labourCost   = ("mazdori ");
+
+// var totalCost = materialCost + labourCost;
+
+// if (totalCost === materialCost + labourCost ){
+//     document.writeln("The Cost Equal")
+// }
+// else (document.writeln("mazdori increase kro ")
+// )
+
+                                                // E      
+
+    // var truely =prompt(" true or false")
+
+    // if (truely === "true"){
+    //     console.log ("Ap Sahi they")
+    // }
+
+    // else
+    // {
+    //     console.log ("Ap abhi bhi Sahi ho me galat hu")
+    // }
+
+                                        // F
+
+    // var value = prompt("car or cat ? which is bigger") 
+
+
+    // if (value === "cat"){
+    //     console.log ("Cat is smaller")
+    // }
+    // else(console.log("car is smaller? r u serious"))
+
+
+                                            // 6 
+
+//                                             let sub1 = "Math"
+// let sub2 = "Urdu"
+// let sub3 = "Islamiyat"
+
+// let totalMarks = 100 *3;
+
+// let firstSubObtainMark = 95;
+// let secSubObtainMark = 95;
+// let thirdSubObtainMark = 95;
+
+// let totalObtainedMarks = firstSubObtainMark + secSubObtainMark + thirdSubObtainMark
+
+
+// let percentage = ( totalObtainedMarks / totalMarks) * 100
+
+
+
+// console.log( "Total Marks--> ",  totalMarks)
+
+// console.log("Total Obtained Marks-->", totalObtainedMarks)
+
+// console.log("Total Percentage", percentage + "%")
+
+
+// document.writeln(`
+//     <table border="2"  class="table-001" style="background-color: black; color: white;">
+//     <th >Subject</th>
+//     <th> Mark</th>
+//     <th>Obtained Mark</th>
+//     <th>Percentage</th>
+//     <tr>
+//         <td>Math </td>
+//         <td>100</td>
+//         <td>${firstSubObtainMark}</td>
+//     </tr>
+//     <tr>
+//         <td>Urdu</td>
+//         <td>100</td>
+//         <td>${secSubObtainMark}</td>
+//     </tr>
+//     <tr>
+//         <td>Islamiyat</td>
+//                 <td>100</td>
+//         <td>${thirdSubObtainMark}</td>
+
+//     </tr>
+//     <tr>
+//         <td></td>
+//         <td>300</td>
+//         <td>${totalObtainedMarks}</td>
+//         <td>${percentage}%</td>
+//     </tr>
+// </table>
+//       `)
+
+
+                                        // 7 
+
+// let number = prompt("Chose the 1-10")
+
+// if(number == 4){
+//     console.log("correct Answer")
+// }
+
+// else if (number == 1){
+//     console.log("Thoda sa better guess")
+// }
+
+// else if (number == 2){
+//     console.log("Thoda sa or Kareeb Ho rha He")
+// }
+
+// else if (number == 3){
+//     console.log("Bas Ek Or Step Door He")
+// }
+
+// else if (number == 5){
+//     console.log("Bas Ek Or Step Door He")
+// }
+
+// else if (number == 6){
+//     console.log("Thoda sa or Try Karo")
+// }
+
+// else if (number == 7){
+//     console.log("Kareeb Ho rha He")
+// }
+
+// else if (number == 8){
+//     console.log("Thoda sa better guess")
+// }
+
+// else if (number == 9){
+//     console.log("low guess")
+// }
+
+// else if (number == 10){
+//     console.log("low guess")
+// }
+
+// else (
+//     console.log("parhna nhi ata kia beta 1-10 likhna he")
+// )
+
+
+                                                // 8 
+
+
+// var number = prompt ("write number to divide it by 3")
+
+// var divide = (number / 3)
+
+// document.writeln ("it can be divide by 3 Here is the answer" , divide )
+
+
+                                                // 9
+
+
+// let number = prompt("Enter a number:");
+
+
+// number = Number(number);
+
+
+// if (isNaN(number)) {
+//   console.log("That's not a valid number!");
+// } else {
+
+//   if (number % 2 === 0) {
+//     console.log(number + " is an even number.");
+//   } else {
+//     console.log(number + " is an odd number.");
+//   }
+// }
+
+
+                                            // 10 
+
+// let temp = prompt("Enter the temperature (C):");
+
+// temp = Number(temp);
+
+
+//   if (temp > 40) {
+
+//     console.log("It is too hot outside.");
+
+//   } else if (temp > 30) {
+
+//     console.log("The Weather today is Normal.");
+
+//   } else if (temp > 20) {
+
+//     console.log("Today's Weather is cool.");
+
+//   } else if (temp > 10) {
+
+//     console.log("OMG! Today's weather is so Cool.");
+
+//   } else {
+
+//     console.log("It's freezing! Stay warm.");
+
+//   }
+
+
+
+//chap no 9 qs 11
+
+
+
+// let num1 = parseFloat(prompt("Enter the Num1:"));
+
+// let num2 = parseFloat(prompt("Enter the Num2:"));
+
+// let operation = prompt("Enter operation = +, -, *, /, %:");
+
+
+
+// let result;
+
+
+
+// if (operation === "+") {
+
+//   result = num1 + num2;
+
+// } 
+
+// else if (operation === "-") {
+
+//   result = num1 - num2;
+
+// } 
+
+// else if (operation === "*") {
+
+//   result = num1 * num2;
+
+// } 
+
+// else if (operation === "/") {
+
+//   if (num2 !== 0) {
+
+//     result = num1 / num2;
+
+//   } else {
+
+//     result = "Error!.";
+
+//   }
+
+// } 
+
+// else if (operation === "%") {
+
+//   if (num2 !== 0) {
+
+//     result = num1 % num2;
+
+//   } else {
+
+//     result = "Error! .";
+
+//   }
+
+// } 
+
+// else {
+
+//   result = "galat operator dala hai";
+
+// }
+
+
+
+// document.writeln ("Result: " + result);                            
+
+
+
+                        //chap no 9 qs 11
+
+// let num1 = parseFloat(prompt("Enter the Num1:"));
+// let num2 = parseFloat(prompt("Enter the Num2:"));
+// let operation = prompt("Enter operation = +, -, *, /, %:");
+
+// let result;
+
+// if (operation === "+") {
+//   result = num1 + num2;
+// } 
+// else if (operation === "-") {
+//   result = num1 - num2;
+// } 
+// else if (operation === "*") {
+//   result = num1 * num2;
+// } 
+// else if (operation === "/") {
+//   if (num2 !== 0) {
+//     result = num1 / num2;
+//   } else {
+//     result = "Error!.";
+//   }
+// } 
+// else if (operation === "%") {
+//   if (num2 !== 0) {
+//     result = num1 % num2;
+//   } else {
+//     result = "Error! .";
+//   }
+// } 
+// else {
+//   result = "galat operator dala hai";
+// }
+
+// document.writeln ("Result: " + result);
